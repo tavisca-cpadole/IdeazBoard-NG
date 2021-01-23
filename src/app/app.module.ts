9722d399c414bd9a129841ecb224d2ce1df00c6e
Chinmay Ashok Padole <cpadole@tavisca.com>
2021-01-23 19:20:06 +0530

    Added implementation for ideazBoard in angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
+import { HomeComponent } from './home/home.component';
+import { StoreModule } from '@ngrx/store';
+import { reducer } from './shared/reducers/card.reducer';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, CardComponent, HomeComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({
+      card: reducer,
+    }),
+    FormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
